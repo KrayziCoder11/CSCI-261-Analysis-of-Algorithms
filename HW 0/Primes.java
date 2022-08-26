f572506b20698df9333661495bde732e9ad7bf6d
KrayziCoder11 <john.dumont11@gmail.com>
2022-08-26 18:39:55 -0400

    Primes

---
+++ HW 0/Primes.java
@@ -0,0 +1,35 @@
+import java.math.*;
+import java.util.Scanner;
+import java.io.*;
+
+public class Primes {
+
+    static boolean isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+  
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+  
+        return true;
+    }
+
+
+    static void printPrimes(int n) {
+        int count = 2;
+        while(count <= n){
+            if(isPrime(count)){
+                System.out.println(count);
+            }
+            count = count + 1;
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
